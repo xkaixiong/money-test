ec9fe37c6c44164300218ac00d51e24dca85a613
kaixiong <258976882@qq.com>
2020-07-27 11:43:16 +0800

    tag组件及toggle功能

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -3,23 +3,28 @@ import Layout from '@/components/Layout.vue';
 <template>
     <Layout class-prefix="layout">
         <NumberBoard/>
-        <AccountTypes :xxx="   333   " />
+        <AccountTypes/>
         <Notes/>
-        <Tags/>
+        <Tags :data-source="tags"/>
     </Layout>
 
 </template>
 
-<script lang="ts">
-  import NumberBoard from '@/components/Accout/NumberBoard.vue';
-  import AccountTypes from '@/components/Accout/AccountTypes.vue';
-  import Notes from '@/components/Accout/Notes.vue';
-  import Tags from '@/components/Accout/Tags.vue';
+<script lang="js">
+    import NumberBoard from '@/components/Accout/NumberBoard.vue';
+    import AccountTypes from '@/components/Accout/AccountTypes.vue';
+    import Notes from '@/components/Accout/Notes.vue';
+    import Tags from '@/components/Accout/Tags.vue';
 
-  export default {
-    name: 'Account',
-    components: {Tags, Notes, AccountTypes, NumberBoard}
-  };
+    export default {
+        name: 'Account',
+        components: {Tags, Notes, AccountTypes, NumberBoard},
+        data() {
+            return {
+                tags: ['衣', '食', '住', '行', '其他']
+            }
+        }
+    };
 </script>
 
 
