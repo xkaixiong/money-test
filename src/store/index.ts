316a3397536f138ff67b5cb82bee840629a9219a
kaixiong <258976882@qq.com>
2020-08-07 21:57:02 +0800

    修复刷新变成404 bug

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,25 +2,54 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from '@/lib/clone';
 import createId from '@/lib/createId';
+import router from '@/router';
 
 Vue.use(Vuex);
 
-type RootState={
-  records:  RecordItem[],
-  tagList:   Tag[],
+type RootState = {
+  records: RecordItem[],
+  tagList: Tag[],
   currentTag?: Tag
 }
-
 const store = new Vuex.Store({
   state: {
-    records: []  ,
-    tagList: []  ,
-    currentTag:undefined
+    records: [],
+    tagList: [],
+    currentTag: undefined
   } as RootState,
   mutations: {
-    setCurrentTag(state,id:string){
-      const id = this.$route.params.id;
+    setCurrentTag(state, id: string) {
       state.currentTag = state.tagList.filter(t => t.id === id)[0];
+    },
+    updateTag(state, payload: { id: string, name: string }) {
+      const {id, name} = payload;
+      const idList = state.tagList.map(item => item.id);
+      if (idList.indexOf(id) >= 0) {
+        const names = state.tagList.map(item => item.name);
+        if (names.indexOf(name) >= 0) {
+          window.alert('标签名重复了');
+        } else {
+          const tag = state.tagList.filter(item => item.id = id)[0];
+          tag.name = name;
+          store.commit('saveTags');
+        }
+      }
+    },
+    removeTag(state, id: string) {
+      let index = -1;
+      for (let i = 0; i < state.tagList.length; i++) {
+        if (state.tagList[i].id === id) {
+          index = i;
+          break;
+        }
+      }
+      if (index >= 0) {
+        state.tagList.splice(index, 1);
+        store.commit('saveTags');
+        router.back();
+      } else {
+        window.alert('删除失败' );
+      }
     },
     fetchRecords(state) {
       state.records = JSON.parse(window.localStorage.getItem('records') || '[]') as RecordItem[];
@@ -30,14 +59,13 @@ const store = new Vuex.Store({
       record2.createdAt = new Date();
       state.records.push(record2);
       store.commit('saveRecords');
-
     },
     saveRecords() {
       window.localStorage.setItem('records',
         JSON.stringify(this.records));
     },
     fetchTags(state) {
-       state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
+      state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
     },
     createTag(state, name: string) {
       const names = state.tagList.map(item => item.name);
