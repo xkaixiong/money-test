5273aff0c38ec6220716a4afd15ab94b2ffd5195
kaixiong <258976882@qq.com>
2020-08-07 21:59:35 +0800

    修复刷新变成404 bug

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+import store from '@/store';
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
 import Icon from '@/components/Icon.vue';
-
 
 Vue.config.productionTip = false;
 
@@ -16,5 +16,6 @@ Vue.component('Icon', Icon);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app');
