1e38f2ff2e8162c99eddc6d6667f670f2e0e1d9d
kaixiong <258976882@qq.com>
2020-08-20 22:32:36 +0800

    修复手误引起的 CSS bug

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -1,11 +1,8 @@
-import Layout from '@/components/Layout.vue';
-
 <template>
   <Layout class-prefix="layout">
     <NumberBoard :value.sync="record.amount" @submit="saveRecord"/>
-    <AccountTypes :value.sync="record.type"/>
     <Tabs :data-source="recordTypeList"
-          :value.sync="record.type"  />
+          :value.sync="record.type"/>
     <div class="notes">
       <FormItem field-name="备注"
                 placeholder="在这里输入备注"
@@ -22,23 +19,25 @@ import NumberBoard from '@/components/Accout/NumberBoard.vue';
 import FormItem from '@/components/Accout/FormItem.vue';
 import Tags from '@/components/Accout/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
 import recordTypeList from '@/constants/recordTypeList';
 
 @Component({
-  components: {Tags, FormItem, NumberBoard},
+  components: {Tags, Tabs, FormItem, NumberBoard},
 })
-
 export default class Account extends Vue {
-get records (){
-  return this.$store.state.records;
-}
-recordTypeList = recordTypeList;
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+
+  recordTypeList = recordTypeList;
+
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
   };
 
-  created(){
-    this.$store.commit('fetchRecords')
+  created() {
+    this.$store.commit('fetchRecords');
   }
 
   onUpdateNotes(value: string) {
@@ -46,17 +45,16 @@ recordTypeList = recordTypeList;
   }
 
   saveRecord() {
-    this.$store.commit('createRecord',this.record);
+    this.$store.commit('createRecord', this.record);
   }
 }
 </script>
 
-<style lang="scss">
-.layout-content {
+<style lang="scss" scoped>
+::v-deep .layout-content {
   display: flex;
   flex-direction: column-reverse;
 }
-
 .note {
   padding: 12px 0;
 }
