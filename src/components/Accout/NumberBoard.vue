03fc68bcb18e8915b2ac75604dfdb8c41f252170
kaixiong <258976882@qq.com>
2020-08-22 15:35:53 +0800

    修复过多的提示

--- src/components/Accout/NumberBoard.vue
+++ src/components/Accout/NumberBoard.vue
@@ -5,11 +5,11 @@
             <button @click="inputContent">7</button>
             <button @click="inputContent">8</button>
             <button @click="inputContent">9</button>
-            <button>-</button>
+            <button @click="inputContent">-</button>
             <button @click="inputContent">4</button>
             <button @click="inputContent">5</button>
             <button @click="inputContent">6</button>
-            <button>+</button>
+            <button @click="inputContent">+</button>
             <button @click="inputContent">1</button>
             <button @click="inputContent">2</button>
             <button @click="inputContent">3</button>
--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,8 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   state: {
     recordList: [],
-    createRecordError:null,
+    createRecordError: null,
+    createTagError:    null,
     tagList: [],
     currentTag: undefined
   } as RootState,
@@ -62,23 +63,24 @@ const store = new Vuex.Store({
     },
     fetchTags(state) {
       state.tagList = JSON.parse(window.localStorage.getItem('tagList') || '[]');
-      if(!state.tagList||state.tagList.length===0){
-        store.commit('createTag','衣')
-        store.commit('createTag','食')
-        store.commit('createTag','住')
-        store.commit('createTag','行')
-        store.commit('createTag','其他')
+      if (!state.tagList || state.tagList.length === 0) {
+        store.commit('createTag', '衣');
+        store.commit('createTag', '食');
+        store.commit('createTag', '住');
+        store.commit('createTag', '行');
+        store.commit('createTag', '其他');
       }
     },
     createTag(state, name: string) {
+      state.createTagError=null;
       const names = state.tagList.map(item => item.name);
       if (names.indexOf(name) >= 0) {
-        window.alert('标签名重复了');
+        state.createTagError = new Error('tag name duplicated');
+        return;
       }
       const id = createId().toString();
       state.tagList.push({id, name: name});
       store.commit('saveTags');
-      window.alert('添加成功');
     },
     saveTags(state) {
       window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
